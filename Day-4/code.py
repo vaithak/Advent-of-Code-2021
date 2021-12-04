5b1c54abfb7502e8b2846d55f726f5c4662678bb
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-12-04 23:48:19 +0530

    Add Day-4

--- Day-4/code.py
+++ Day-4/code.py
@@ -1,30 +1,19 @@
 import numpy as np
 
-def get_boards_and_numbers():
-    with open('input.txt') as f:    # added an extra newline
-        line = f.readline().strip()
-        # read numbers into numbers
-        numbers = [int(x) for x in line.split(',')]
 
-        # empty line
-        f.readline()
-
-        line_num = 0
-        boards_arr = []
-        curr_board = []
-        for line in f:
-            line_num += 1
-            if line_num % 6 == 0:
-                boards_arr.append(curr_board)
-                curr_board = []
-                continue
-            # read 5 numbers into curr_board
-            curr_board.append([int(x) for x in line.split(' ') if x.strip().isdigit()])
-
-    print(numbers)
-    print(boards_arr[0])
-    print(boards_arr[len(boards_arr)-1])
-    return numbers, boards_arr
+def boards_generator(file):
+    line_num = 0
+    board = []
+    for line in file:
+        line_num += 1
+        # read 5 numbers into curr_board
+        board.append([int(x) for x in line.split(' ') if x.strip().isdigit()])
+        if line_num % 5 == 0:
+            yield board
+            # empty line.
+            file.readline()
+            # will create a new board now.
+            board = []
 
 
 def is_win(bit_board):
@@ -39,12 +28,12 @@ def is_win(bit_board):
     return False
 
 
-def calculate_time_and_score(numbers, board):
+def calculate_time_and_score(numbers, board, direction):
     # create hash map from number to position for board
     positions = {}
     for i in range(5):
         for j in range(5):
-            positions[board[i,j]] = (i,j)
+            positions[board[i][j]] = (i,j)
 
     # create binary matrix for checking win
     bit_board = np.zeros((5,5))
@@ -54,28 +43,34 @@ def calculate_time_and_score(numbers, board):
         curr_num = numbers[t]
         if curr_num in positions:
             p = positions[curr_num]
-            bit_board[p.first, p.second] = 1.0
+            bit_board[p[0], p[1]] = 1.0
             sum -= curr_num
             if is_win(bit_board):
                 return t, sum*curr_num
 
-    return 10000, 0
+    return direction*10000, 0
 
 
-def F():
-    numbers, boards_arr = get_boards_and_numbers()
-    best_time, best_score = 10000, 0
-    for board in boards_arr:
-        curr_time, curr_score = calculate_time_and_score(numbers, board)
-        if curr_time < best_time:
-            best_time = curr_time
-            best_score = curr_score
-    print("\tbest time : ", best_time)
-    print("\tbest score: ", best_score)
+def F (direction = 1):
+    with open('input.txt') as f:
+        # read numbers.
+        line = f.readline().strip()
+        numbers = [int(x) for x in line.split(',')]
+        # read empty line.
+        f.readline()
+        # board generator
+        best_time, best_score = direction*10000, 0
+        for board in boards_generator(f):
+            curr_time, curr_score = calculate_time_and_score(numbers, board, direction)
+            if direction*curr_time < direction*best_time:
+                best_time = curr_time
+                best_score = curr_score
+        print("\tbest time : ", best_time)
+        print("\tbest score: ", best_score)
             
 
 print("Part 1:")
-F()
+F(direction = 1)
 
 print("Part 2:")
-F()
+F(direction = -1)
