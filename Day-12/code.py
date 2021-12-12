8e2c286a59ff33e9646010eee75ceb605bc1bee4
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-12-12 20:29:23 +0530

    Day-12 finished :tada:

--- Day-12/code.py
+++ Day-12/code.py
@@ -1,27 +1,53 @@
-def dfs_routes(adjList, start_node, end_node, visited):
+def dfs_routes(adjList, start_node, end_node, visited, small_twice_node=None):
     if start_node[0].islower():
-        visited[start_node] = True
+        visited[start_node] += 1
     res = 0
     for neighbor in adjList[start_node]:
         if neighbor == end_node:
             res += 1
-        elif visited[neighbor] == False:
+        elif visited[neighbor] != 1:
             res += dfs_routes(adjList, neighbor, end_node, visited)
-    visited[start_node] = False
+    if start_node.islower():
+        visited[start_node] -= 1
     return res
 
-
-adjList = {}
-with open("input.txt") as f:
-    for line in f:
-        # form adjacency list
-        a, b = line.strip().split("-") # edge a-b
-        if a in adjList:    adjList[a].append(b)
-        else:               adjList[a] = [b]
-        if b in adjList:    adjList[b].append(a)
-        else:               adjList[b] = [a]
+def form_adj_list():
+    adjList = {}
+    with open("input.txt") as f:
+        for line in f:
+            # form adjacency list
+            a, b = line.strip().split("-") # edge a-b
+            if a in adjList:    adjList[a].append(b)
+            else:               adjList[a] = [b]
+            if b in adjList:    adjList[b].append(a)
+            else:               adjList[b] = [a]
+    return adjList
+   
+def F_1():
+    adjList = form_adj_list()
     visited = {}
     for node in adjList.keys():
-        visited[node] = False
-    print(adjList)
-    print(dfs_routes(adjList, "start", "end", visited))
+        visited[node] = 0
+    print("\t", dfs_routes(adjList, "start", "end", visited))
+
+print("Part 1:")
+F_1()
+
+
+### Part 2:
+def F_2():
+    adjList = form_adj_list()
+    visited = {}
+    for node in adjList.keys():
+        visited[node] = 0
+    initial_res = dfs_routes(adjList, "start", "end", visited)
+    res = initial_res
+    for node in adjList.keys():
+        if node.islower() and node not in ["start", "end"]:
+            visited[node] = -1
+            res += dfs_routes(adjList, "start", "end", visited) - initial_res
+            visited[node] = 0
+    print("\t", res)
+    
+print("Part 2:")
+F_2()
